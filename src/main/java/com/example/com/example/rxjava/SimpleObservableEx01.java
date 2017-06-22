34f592bc2881ad505e2dfa7302f02b5a8a5373ec
Robert Wood <rob.a.wood@gmail.com>
2017-06-22 07:45:37 -0600

    Add examples 01 - 05

--- src/main/java/com/example/com/example/rxjava/SimpleObservableEx01.java
+++ src/main/java/com/example/com/example/rxjava/SimpleObservableEx01.java
@@ -18,7 +18,8 @@ public class SimpleObservableEx01 {
     }
 
     // http://reactivex.io/documentation/operators/just.html   RxJava1.x
-    public void getValues_just_01() {
+    public void example00() {
+        System.out.println("\nStart: example00");
         Observable.just(1, 2, 3)
                 .subscribe(new Subscriber<Integer>() {
                     //public void onSubscribe(Subscription )
@@ -44,7 +45,106 @@ public class SimpleObservableEx01 {
         return "Hello";
     }
 
+    public void example01() {
+        System.out.println("\nStart: example01");
+        Observable<Integer> obs = Observable.create(s -> {
+            s.onNext(1);
+            s.onNext(2);
+            s.onNext(3);
+            //  Only useful if Subscriber implements onCompleted(),
+            //    but lambda implementation below seems to only implement onNext()
+            //s.onCompleted();
+        });
 
+        obs.map(i -> "Number: " + i)
+                .subscribe(s -> System.out.println(s));
+
+        // Verify: can "reuse" an Observable
+        obs.map(i -> "Round 2 Number: " + i)
+                .subscribe(s -> System.out.println(s));
+    }
+
+    public void example02() {
+        System.out.println("\nStart: example02");
+        Observable<Integer> obs = Observable.create(s -> {
+            for (int ix = 0; ix < 4; ix++) {
+                s.onNext(ix);
+            }
+            //s.onCompleted();
+        });
+
+        obs.subscribe(s -> System.out.println(s));
+    }
+
+    public void example03() {
+        System.out.println("\nStart: example03");
+        Observable<Integer> obs = Observable.create(s -> {
+            for (int ix = 0; ix < 10; ix++) {
+                s.onNext(ix);
+            }
+            //s.onCompleted();
+        });
+
+        obs.filter(i -> i % 2 == 0)
+                .subscribe(s -> System.out.println(s));
+    }
+
+    public void example04() {
+        System.out.println("\nStart: example04");
+        Observable<Integer> obs = Observable.create(s -> {
+            for (int ix = 0; ix < 10; ix++) {
+                s.onNext(ix);
+            }
+            s.onCompleted();
+        });
+
+        obs.filter(i -> i % 2 == 0)
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("End: example04\n");
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        System.out.println("Error: " + throwable.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        System.out.println("Number: " + integer);
+                    }
+                });
+    }
+
+    public void example05() {
+        System.out.println("\nStart: example05");
+        Observable<Integer> obs = Observable.create(s -> {
+            for (int ix = 0; ix < 10; ix++) {
+                s.onNext(ix);
+            }
+            s.onCompleted();
+        });
+
+        obs.filter(i -> i % 2 == 0)
+                .map(i -> "Mapped Number " + i)
+                .subscribe(new Subscriber<String>() {
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("End: example05\n");
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        System.out.println("Error: " + throwable.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(String text) {
+                        System.out.println(text);
+                    }
+                });
+    }
 
     public static void main(String[] args) {
         SimpleObservableEx01 tester = new SimpleObservableEx01();
@@ -52,6 +152,16 @@ public class SimpleObservableEx01 {
         String message = tester.getMessage_just_01();
         System.out.println("Message: " + message);
 
-        tester.getValues_just_01();
+        tester.example00();
+
+        tester.example01();
+
+        tester.example02();
+
+        tester.example03();
+
+        tester.example04();
+
+        tester.example05();
     }
 }
